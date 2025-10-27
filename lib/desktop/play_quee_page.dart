e692fbabbcaee487a0ceaec75c05c3dc2a0ae019
AfalpHy <736353503@qq.com>
2025-10-27 12:31:04 +0800

    support remove song from play queue on desktop

--- lib/desktop/play_quee_page.dart
+++ lib/desktop/play_quee_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:particle_music/common.dart';
 import 'package:particle_music/cover_art_widget.dart';
+import 'package:super_context_menu/super_context_menu.dart';
 import '../audio_handler.dart';
 
 class PlayQueuePage extends StatefulWidget {
@@ -46,6 +47,7 @@ class PlayQueueSheetState extends State<PlayQueuePage> {
                     audioHandler.clear();
 
                     setState(() {});
+                    widget.displayPlayQueuePageNotifier.value = false;
                   }
                 },
                 icon: const ImageIcon(deleteImage, color: Colors.black),
@@ -87,51 +89,80 @@ class PlayQueueSheetState extends State<PlayQueuePage> {
             itemCount: playQueue.length,
             itemBuilder: (_, index) {
               final song = playQueue[index];
-              return ReorderableDelayedDragStartListener(
+              return ContextMenuWidget(
                 key: ValueKey(index),
-                index: index,
-                child: ValueListenableBuilder(
-                  valueListenable: currentSongNotifier,
-                  builder: (_, currentSong, _) {
-                    return Center(
-                      child: ListTile(
-                        leading: CoverArtWidget(
-                          size: 50,
-                          borderRadius: 5,
-                          source: getCoverArt(song),
-                        ),
-                        title: Text(
-                          getTitle(song),
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: song == currentSong
-                                ? Color.fromARGB(255, 75, 210, 210)
-                                : null,
-                            fontWeight: song == currentSong
-                                ? FontWeight.bold
-                                : null,
-                            fontSize: 14,
+                child: ReorderableDragStartListener(
+                  index: index,
+                  child: ValueListenableBuilder(
+                    valueListenable: currentSongNotifier,
+                    builder: (_, currentSong, _) {
+                      return Center(
+                        child: ListTile(
+                          leading: CoverArtWidget(
+                            size: 50,
+                            borderRadius: 5,
+                            source: getCoverArt(song),
                           ),
+                          title: Text(
+                            getTitle(song),
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: song == currentSong
+                                  ? Color.fromARGB(255, 75, 210, 210)
+                                  : null,
+                              fontWeight: song == currentSong
+                                  ? FontWeight.bold
+                                  : null,
+                              fontSize: 14,
+                            ),
+                          ),
+                          subtitle: Text(
+                            "${getArtist(song)} - ${getAlbum(song)}",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 12, color: Colors.grey),
+                          ),
+                          trailing: Text(
+                            formatDuration(getDuration(song)),
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          onTap: () async {
+                            audioHandler.currentIndex = index;
+                            await audioHandler.load();
+                            await audioHandler.play();
+                          },
                         ),
-                        subtitle: Text(
-                          "${getArtist(song)} - ${getAlbum(song)}",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 12, color: Colors.grey),
-                        ),
-                        trailing: Text(
-                          formatDuration(getDuration(song)),
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                        onTap: () async {
-                          audioHandler.currentIndex = index;
-                          await audioHandler.load();
-                          await audioHandler.play();
+                      );
+                    },
+                  ),
+                ),
+                menuProvider: (request) {
+                  return Menu(
+                    children: [
+                      MenuAction(
+                        title: 'Remove',
+                        image: MenuImage.icon(Icons.close_rounded),
+                        callback: () async {
+                          audioHandler.delete(index);
+                          setState(() {});
+                          if (index < audioHandler.currentIndex) {
+                            audioHandler.currentIndex -= 1;
+                          } else if (index == audioHandler.currentIndex) {
+                            if (playQueue.isEmpty) {
+                              audioHandler.clear();
+                              widget.displayPlayQueuePageNotifier.value = false;
+                            } else {
+                              if (index == playQueue.length) {
+                                audioHandler.currentIndex = 0;
+                              }
+                              await audioHandler.load();
+                            }
+                          }
                         },
                       ),
-                    );
-                  },
-                ),
+                    ],
+                  );
+                },
               );
             },
           ),
