5c9374359db359a854751b8cf5ae352bce424718
AfalpHy <736353503@qq.com>
2026-03-09 11:18:12 +0800

    feat: tap outside to hide keyboard

--- lib/mobile/widgets/my_search_field.dart
+++ lib/mobile/widgets/my_search_field.dart
@@ -26,39 +26,44 @@ class MySearchField extends StatelessWidget {
                   padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
                   child: SizedBox(
                     height: 30,
-                    child: TextField(
-                      autofocus: true,
-                      controller: textController,
-                      style: TextStyle(color: textColor),
-                      decoration: InputDecoration(
-                        hint: Text(
-                          AppLocalizations.of(context).searchSongs,
-                          style: TextStyle(color: textColor),
-                        ),
-                        prefixIcon: Icon(Icons.search, color: iconColor),
-                        suffixIcon: IconButton(
-                          color: iconColor,
-                          onPressed: () {
-                            isSearch.value = false;
-                            textController.clear();
-                            FocusScope.of(context).unfocus();
-                            onSearchTextChanged();
-                          },
-                          icon: const Icon(Icons.clear),
-                          padding: EdgeInsets.zero,
-                        ),
-                        filled: true,
-                        fillColor: searchFieldColor,
-                        contentPadding: EdgeInsets.zero,
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      onChanged: (value) {
-                        onSearchTextChanged();
+                    child: TapRegion(
+                      onTapOutside: (_) {
+                        FocusManager.instance.primaryFocus?.unfocus();
                       },
+                      child: TextField(
+                        autofocus: true,
+                        controller: textController,
+                        style: TextStyle(color: textColor),
+                        decoration: InputDecoration(
+                          hint: Text(
+                            AppLocalizations.of(context).searchSongs,
+                            style: TextStyle(color: textColor),
+                          ),
+                          prefixIcon: Icon(Icons.search, color: iconColor),
+                          suffixIcon: IconButton(
+                            color: iconColor,
+                            onPressed: () {
+                              isSearch.value = false;
+                              textController.clear();
+                              FocusScope.of(context).unfocus();
+                              onSearchTextChanged();
+                            },
+                            icon: const Icon(Icons.clear),
+                            padding: EdgeInsets.zero,
+                          ),
+                          filled: true,
+                          fillColor: searchFieldColor,
+                          contentPadding: EdgeInsets.zero,
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        onChanged: (value) {
+                          onSearchTextChanged();
+                        },
+                      ),
                     ),
                   ),
                 ),
