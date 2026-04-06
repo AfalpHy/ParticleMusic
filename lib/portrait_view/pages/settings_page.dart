ecbf48c7a4b4f04aa897513e40c6ec7a79be826c
AfalpHy <736353503@qq.com>
2026-04-06 17:11:20 +0800

    trivial

--- lib/portrait_view/pages/settings_page.dart
+++ lib/portrait_view/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:particle_music/common_widgets/settings_list.dart';
+import 'package:particle_music/portrait_view/custom_appbar_leading.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -11,6 +12,7 @@ class SettingsPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         automaticallyImplyLeading: false,
+        leading: customAppBarLeading(context),
 
         backgroundColor: Colors.transparent,
         elevation: 0,
