77cc5e7b25983760560465925bd68f71c358318e
AfalpHy <736353503@qq.com>
2026-08-04 16:42:51 +0800

    chore: bump version to 3.5.3

--- generate_deb.sh
+++ generate_deb.sh
@@ -3,7 +3,7 @@
 # ---------- CONFIG ----------
 BINARY_NAME="sylvakru"
 APP_NAME="Sylvakru"
-APP_VERSION="3.5.2"
+APP_VERSION="3.5.3"
 MAINTAINER="AfalpHy"
 
 BUNDLE_DIR="build/linux/x64/release/bundle"
