dc08631650663809096ad2f9632f3f49166c36b8
AfalpHy <736353503@qq.com>
2026-08-13 12:06:50 +0800

    chore: bump version to 3.6.0

--- generate_deb.sh
+++ generate_deb.sh
@@ -3,7 +3,7 @@
 # ---------- CONFIG ----------
 BINARY_NAME="sylvakru"
 APP_NAME="Sylvakru"
-APP_VERSION="3.5.3"
+APP_VERSION="3.6.0"
 MAINTAINER="AfalpHy"
 
 BUNDLE_DIR="build/linux/x64/release/bundle"
